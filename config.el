63e5d0ea145b139e725720beb30a5fee9f16910b
Michael Xavier <michael@michaelxavier.net>
2022-03-18 22:27:57 -0700

    Change theme to challenger-deep

--- config.el
+++ config.el
@@ -26,7 +26,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; other good ones so far are are doom-homage-black, doom-plain-dark
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-challenger-deep)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
