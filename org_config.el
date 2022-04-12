cab22cb47ca89f38bf8bc297ecd6c4d9f69f1ff8
Michael Xavier <michael@michaelxavier.net>
2022-04-11 21:02:15 -0700

    Remove ace jump org mode rebind
    I think this was leftover from my old emacs config. I think doom-emacs
    bindings obviate this.

--- org_config.el
+++ org_config.el
@@ -1,9 +1,5 @@
 (setq org-directory "~/Dropbox/org/")
 
-;; take back C-c SPC
-(map! :mode org-mode-map
-      "C-c SPC" #'ace-jump-mode)
-
 (defun org-archive-done-tasks ()
   "Archive done tasks in the current org mode file"
   (interactive)
