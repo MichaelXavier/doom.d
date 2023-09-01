08c86738a712a36dad47c01613435c4ff00cc34b
Michael Xavier <michael@michaelxavier.net>
2023-09-01 08:57:50 -0700

    Workaround weird font lock issue in ESS

--- r_config.el
+++ r_config.el
@@ -1,4 +1,11 @@
 ;;; r_config.el -*- lexical-binding: t; -*-
 
 (use-package! ess
-  :commands (R r-mode R-mode ess-mode))
+  :commands (R r-mode R-mode ess-mode)
+  :config
+  (defun my-inferior-ess-init ()
+    "Workaround for https://github.com/emacs-ess/ESS/issues/1193"
+    (add-hook 'comint-preoutput-filter-functions #'xterm-color-filter -90 t)
+    (setq-local ansi-color-for-comint-mode nil))
+  (add-hook 'inferior-ess-mode-hook #'my-inferior-ess-init)
+  )
