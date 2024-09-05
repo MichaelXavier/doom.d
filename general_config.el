8197a53c02aff138eae31e4438868a7cc4187209
Michael Xavier <michael.xavier@well.co>
2024-09-05 14:09:57 -0700

    Add alternate edwina bindings
    Going to be changing to an independent tiling window manager so i want
    to remember how to navigate within and without emacs

--- general_config.el
+++ general_config.el
@@ -814,6 +814,9 @@
   :hook (edwina-mode . my/edwina-remove-or-restore-buffer-behavior)
   :bind (("C-S-p" . edwina-select-previous-window)
          ("C-S-n" . edwina-select-next-window)
+         ;; Try to get closer to my window manager's bindings
+         ("C-S-j" . edwina-select-next-window)
+         ("C-S-k" . edwina-select-previous-window)
          )
   :config
   (map! :map edwina-mode-map
